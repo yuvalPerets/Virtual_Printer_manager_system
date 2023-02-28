a62d486f130deb384af413c3b0daae560c232e1f
yuvalPerets <120096334+yuvalPerets@users.noreply.github.com>
2023-02-28 11:10:04 +0200

    adding the second project into git
    adding the second project into git , this project is the rabbitMQ producer 
    we are using it as the client that can commnicate wtih the printer server

---
+++ virtualPrinterV_1/virtualPrinterV_1/src/main/java/Yuval_Almog/virtualPrinterV_1/VirtualPrinterV1Application.java
@@ -0,0 +1,60 @@
+package Yuval_Almog.virtualPrinterV_1;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ExitCodeEvent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+@SpringBootApplication
+public class VirtualPrinterV1Application {
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+
+	public static void main(String[] args)throws IOException, TimeoutException {
+		SpringApplication.run(VirtualPrinterV1Application.class, args);
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare("testing", false, false, false, null);
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("Select an option:");
+			System.out.println("1. select printer to print");
+			System.out.println("2.random printer print" );
+			System.out.println("3. Quit");
+			int option = scanner.nextInt();
+			if (option == 1) {
+				System.out.print("Enter printer id: ");
+				String message = scanner.next();
+				channel.basicPublish("", "testing", null, message.getBytes());
+				System.out.println("Sent message: " + message);
+			} else if (option ==2) {
+				String message = "random";
+				channel.basicPublish("", "testing", null, message.getBytes());
+				System.out.println("Sent message: " + message);
+			}else if (option == 3) {
+				System.exit(0);
+				break;
+			} else {
+				System.out.println("Invalid option!");
+			}
+		}
+
+
+
+
+
+	}
+
+
+
+}
---
+++ virtualPrinterV_1/virtualPrinterV_1/src/test/java/Yuval_Almog/virtualPrinterV_1/VirtualPrinterV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Yuval_Almog.virtualPrinterV_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPrinterV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
