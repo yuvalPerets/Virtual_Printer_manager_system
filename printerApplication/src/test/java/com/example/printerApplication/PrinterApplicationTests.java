157c8a99684fff9886cdf67d4ab4c3df438d771d
אלמוג רבני <ralmog98@gmail.com>
2023-03-01 12:27:40 +0200

    adding unit test for job

--- printerApplication/src/test/java/com/example/printerApplication/PrinterApplicationTests.java
+++ printerApplication/src/test/java/com/example/printerApplication/PrinterApplicationTests.java
@@ -6,12 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PrinterApplicationTests {
 
-	@Test
-	void Test_Liveness() {
-        Printer p = new Printer(Long.valueOf(100));
-
-
-	}
-
 
 }
